018843a374232940ae9689a226dc090667585030
martin-montas <martinmontas1@gmail.com>
2026-06-28 09:02:42 -0400

    COMMIT

--- src/particle.hpp
+++ src/particle.hpp
@@ -5,14 +5,32 @@
 
 #define RADIUS 10
 
+enum ParticleType { NORMAL, ATTRACTED, REPULSED };
+
 class Particle {
-    int           _x;        /* horizontal coordinate */
-    int           _y;        /* vertical coordinate */
-    int           _width;    /*  how wide the particle */
-    int           _height;   /* how tall the particle */
-    SDL_Renderer* _renderer; /* pointer the renderer */
+    int           _x;         /* horizontal coordinate */
+    int           _y;         /* vertical coordinate */
+    int           _width;     /*  how wide the particle */
+    int           _height;    /* how tall the particle */
+    SDL_Renderer* _renderer;  /* pointer the renderer */
+    Uint8         _r, _g, _b; /* color for the given particle */
+    ParticleType  type;       /* type particle */
+    float         _ax;        /* atractor point x */
+    float         _ay;        /* atractor point y */
+    float         _dx;        /* direction point x */
+    float         _dy;        /* direction point y */
 
   public:
+    inline explicit Particle(int x, int y, int w, int h, Uint8 r, int g, int b, ParticleType t) {
+        _x      = x;
+        _y      = y;
+        _width  = w;
+        _height = h;
+        _r      = r;
+        _g      = g;
+        _b      = b;
+        type    = t;
+    }
     void draw_particle(SDL_Renderer* renderer, int cx, int cy);
 };
 
