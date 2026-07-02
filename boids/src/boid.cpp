ed6cb6bbc0a3c4ac0408fb1e6de645a2b44473a5
martin-montas <martinmontas1@gmail.com>
2026-07-02 00:56:54 -0400

    DEBUGGING: just a commit

--- boids/src/boid.cpp
+++ boids/src/boid.cpp
@@ -6,20 +6,66 @@
 #include <cstdio>
 #include <vector>
 
-void Boid::update(std::vector<Boid *> swarm) {
-  // this fomula calculates the radius of the flock
-  // Formula: (max(abs(x_1 - x_2) , abs(y_1 - y_2)) <= 2;  2 being the radius
-  for (auto b : swarm) {
-    if (b != this) {
-      if (max(abs(x - b->x), abs(y - b->y)) <= 4) {
-      }
+void Boid::update(std::vector<Boid*> swarm) {
+    // this fomula calculates the radius of the flock
+    // Formula: (max(abs(x_1 - x_2) , abs(y_1 - y_2)) <= 2;  2 being the radius
+
+    Vec2  direction = {(float)_ver[0].x - _ver[1].x, (float)_ver[0].y - _ver[1].y};
+    float distance  = std::sqrt((direction.x * direction.x) + (direction.y * direction.y));
+    Vec2  norm      = {direction.x / distance, direction.y / distance};
+
+    int i = 0;
+    int x;
+    int y;
+    while (i <= 2) {
+        x = _ver[i].x * cos(1) - _ver[i].y * sin(1);
+        y = _ver[i].x * sin(1) + _ver[i].y * cos(1);
+
+        _ver[i].x += x;
+        _ver[i].y += y;
+
+        i += 1;
     }
-  }
+
+    check_doundries();
+
+    // for (auto b : swarm) {
+    //     if (b != this) {
+    //         // takes the
+    //         if (max(abs(x - b->x), abs(y - b->y)) <= FLOCK_RADIUS) {
+    //         }
+    //     }
+    // }
+}
+
+void Boid::check_doundries() {
+
+    if (_ver[0].x <= 0) {
+        _ver[0].x = WIDTH;
+    } else if (_ver[1].x <= 0) {
+        _ver[1].x = WIDTH;
+    } else if (_ver[2].x <= 0) {
+        _ver[2].x = WIDTH;
+
+    } else if (_ver[0].y >= HEIGHT) {
+        _ver[0].y = 0;
+    } else if (_ver[1].y >= HEIGHT) {
+        _ver[1].y = 0;
+    } else if (_ver[2].y >= HEIGHT) {
+        _ver[2].y = 0;
+
+    } else if (_ver[0].x >= WIDTH) {
+        _ver[0].y = 0;
+    } else if (_ver[1].x >= WIDTH) {
+        _ver[1].y = 0;
+    } else if (_ver[2].x >= WIDTH) {
+        _ver[2].y = 0;
+    }
 }
 
 void Boid::draw() {
 
-  SDL_RenderDrawLine(_rend, _ver[0].x, _ver[0].y, _ver[1].x, _ver[1].y);
-  SDL_RenderDrawLine(_rend, _ver[1].x, _ver[1].y, _ver[2].x, _ver[2].y);
-  SDL_RenderDrawLine(_rend, _ver[2].x, _ver[2].y, _ver[0].x, _ver[0].y);
+    SDL_RenderDrawLine(_rend, _ver[0].x, _ver[0].y, _ver[1].x, _ver[1].y);
+    SDL_RenderDrawLine(_rend, _ver[1].x, _ver[1].y, _ver[2].x, _ver[2].y);
+    SDL_RenderDrawLine(_rend, _ver[2].x, _ver[2].y, _ver[0].x, _ver[0].y);
 }
--- boids/src/boid.hpp
+++ boids/src/boid.hpp
@@ -5,47 +5,46 @@
 #include <random>
 #include <vector>
 
-#define BETA 0.3
-#define RADIUS 10
-#define BOID_WIDTH 5
-#define BOID_HEIGHT 5
-#define K 0.05
-#define FRICTION 0.85;
+#define RADIUS       10
+#define FLOCK_RADIUS 4
+#define K            0.05
+#define FRICTION     0.85;
 
 using namespace std;
 
 struct Vec2 {
-  float x, y;
-  Vec2 &operator+=(const Vec2 &other) {
-    x += other.x;
-    y += other.y;
-    return *this;
-  }
+    float x, y;
+    Vec2& operator+=(const Vec2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 
-  Vec2 &operator*=(const Vec2 &other) {
-    x *= other.x;
-    y *= other.y;
-    return *this;
-  }
+    Vec2& operator*=(const Vec2& other) {
+        x *= other.x;
+        y *= other.y;
+        return *this;
+    }
 };
 
 class Boid {
-  SDL_Renderer *_rend; /* pointer to renderer object */
+    SDL_Renderer* _rend; /* pointer to renderer object */
 
-public:
-  int x;             /* coodinate x */
-  int y;             /* coodinate y */
-  SDL_Point _ver[3]; /* pointer to vertexs */
+  public:
+    int       x;       /* coodinate x */
+    int       y;       /* coodinate y */
+    SDL_Point _ver[3]; /* pointer to vertexs */
 
-  inline Boid(SDL_Renderer *rend, SDL_Point p1, SDL_Point p2, SDL_Point p3) {
-    _rend = rend;
-    _ver[0] = p1;
-    _ver[1] = p2;
-    _ver[2] = p3;
-  }
+    inline Boid(SDL_Renderer* rend, SDL_Point p1, SDL_Point p2, SDL_Point p3) {
+        _rend   = rend;
+        _ver[0] = p1;
+        _ver[1] = p2;
+        _ver[2] = p3;
+    }
 
-  void draw();
-  void update(std::vector<Boid *> swarm);
+    void draw();
+    void update(std::vector<Boid*> swarm);
+    void check_doundries();
 };
 
 #endif // SRC_BOID_HPP_
